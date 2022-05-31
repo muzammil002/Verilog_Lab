de58c8fed18c0ecd5df9b81f7263b1f99b1bc2d0
Muzammil <66831751+muzammil002@users.noreply.github.com>
2022-05-31 14:45:23 +0500

    Add files via upload

---
+++ part4/part4.sv
@@ -0,0 +1,18 @@
+module part4 (
+	input [1:0] SW,
+	output [6:0] HEX0);
+	
+	assign HEX0[0] = (~SW[1] & ~SW[0]) | (SW[1] & ~SW[0]) | (SW[1] & SW[0]);
+	
+	assign HEX0[1] = (~SW[1] & SW[0]) | (SW[1] & SW[0]);
+	
+	assign HEX0[2] = (~SW[1] & SW[0]) | (SW[1] & SW[0]);
+	
+	assign HEX0[3] = (SW[1] & ~SW[0]) | (SW[1] & SW[0]);
+	
+	assign HEX0[4] = (SW[1] & ~SW[0]) | (SW[1] & SW[0]);
+	
+	assign HEX0[5] = (~SW[1] & ~SW[0]) | (SW[1] & ~SW[0]) | (SW[1] & SW[0]);
+	
+	assign HEX0[6] = (SW[1] & ~SW[0]) | (SW[1] & SW[0]);
+endmodule
